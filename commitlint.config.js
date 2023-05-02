fa6106b6e9f071073ec4dcf2b06adddcd70e55fc
Ecaknight <1561557640@qq.com>
2023-05-02 15:59:18 +0800

    feat: 添加包体积分析插件

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,2 +1,2 @@
-// commit-lint config
+// commit-lint config https://github.com/conventional-changelog/commitlint/#what-is-commitlint
 module.exports = { extends: ['@commitlint/config-conventional'] }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import vueJSX from '@vitejs/plugin-vue-jsx'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJSX()],
+  plugins: [vue(), vueJSX(), visualizer({ filename: 'visualizerFile.html' })],
   server: {
     port: 8080,
     host: '0.0.0.0',
